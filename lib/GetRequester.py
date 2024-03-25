2d4de66f3029a6b10570c33bd7c677c4a4ffca23
maurice diong <mdiong00@gmail.com>
2024-03-24 21:26:08 -0400

    send get request

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        personel_list = []
+        personel = json.loads(self.get_response_body())
+        for person in personel:
+            personel_list.append(personel)
+        
+        return personel
+
+# my_personel = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+
+# print(my_personel.load_json())
